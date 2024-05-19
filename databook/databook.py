13722075445a1b578f214ede635f138471bff8ae
Hamayama <fkyo0985@gmail.com>
2024-05-20 07:49:08 +0900

    app.yaml chg

--- databook/databook.py
+++ databook/databook.py
@@ -23,7 +23,7 @@ from google.appengine.api import search
 from google.appengine.api import capabilities
 
 # databook.py
-# 2024-5-7 v1.52
+# 2024-5-20 v1.53
 
 # Google App Engine / Python による データベース アプリケーション1
 
